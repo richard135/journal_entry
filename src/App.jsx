de25fe2344d0f1a0e69265810911ffc8914251c1
richard135 <richardhsieh1@hotmail.com>
2017-04-03 05:55:15 +0000

    Styled with sass

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ArticleList from './components/ArticleHandlers.jsx';
 import {connect} from 'react-redux';
 import {fetchArticles} from './actions/article.actions.js'
+
 class App extends React.Component{
   componentDidMount(){
     this.props.onFetchArticles();
   }
   render(){
     return <div>
-      <h2>Please enter your article</h2>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand">Journal Entry Sentiment Analysis</a>
+          </div>
+        </div>
+      </nav>
       <ArticleList/>
     </div>
   }
@@ -25,3 +32,4 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
+require ('../../styles/application.scss');
 
 const ArticleList = ({articles, newArticle, onAdd, onUpdateName, onUpdateRating}) => {
   const articleItems = articles.map((article) => {
-    return <div key={article.id}>
-      <p>Article: {article.name} - Word Count:{article.words} - Rating: {article.rating} - Sentiment Score: {article.sentiment_score}</p>
-      <hr/>
+    return <div className='top-border' key={article.id}>
+      <div className="custom-container">
+        <div className='panel-body'>{article.name}</div>
+        <div className="panel-footer">
+          <span className="label label-default">SENTIMENT SCORE: {article.sentiment_score}</span>
+        </div>
+      </div>
     </div>
   });
 
   const votingInteger = [];
   for (let i = 10; i > -11 ; i--) {
-    votingInteger.push(<option value= {i}> {i} </option>)
+    votingInteger.push(<option key={i} value= {i}> {i} </option>)
   }
 
 
   const listSection =
     (<div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        onAdd();
-      }}>
-        <label className='name'>Article Input Here</label>
-        <input type='textarea' name='name' value={newArticle.name}
-          onChange={(e) => onUpdateName(e.target.value)}/>
-        <select className="select" name='select' onChange={(e) => onUpdateRating(e.target.value)}>
-          <optgroup label="Select Table">
-          {votingInteger}
-          </optgroup>
-        </select>
-        <button type='submit'>Create</button>
-      </form>
-      <ul>{articleItems}</ul>
+      <div className="widget-area no-padding blank">
+        <div className="status-upload">
+          <div className="form-article">
+            <section className = "panel">
+              <form onSubmit={(e) => {
+                e.preventDefault();
+                onAdd();
+              }}>
+                <h2>Your Article Here!</h2>
+                <textarea type='textarea' value={newArticle.name}
+                  onChange={(e) => onUpdateName(e.target.value)}/>
+                  <select className='drop-bar' onChange={(e) => onUpdateRating(e.target.value)}>
+                    <optgroup label="HAPPINESS LEVEL">
+                    {votingInteger}
+                    </optgroup>
+                  </select>
+                <button type='submit' className="btn btn-success green">CREATE</button>
+              </form>
+            </section>
+            <section classID="postlist">
+              <div className="panel">
+                {articleItems}
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>)
     return <div>
       {listSection}
@@ -38,4 +55,7 @@ const ArticleList = ({articles, newArticle, onAdd, onUpdateName, onUpdateRating}
 };
 
 export default ArticleList;
+
+
+
 
